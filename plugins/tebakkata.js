04fa96955eff209f2ae67af9c7e8debd7a333a38
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-04-26 22:49:39 +0700

    Update tebakkata.js

--- plugins/tebakkata.js
+++ plugins/tebakkata.js
@@ -1,35 +1,32 @@
-let fetch = require('node-fetch')
+const fetch = require('node-fetch')
 let timeout = 120000
 let poin = 500
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
-    let id = m.chat
-    if (id in conn.tebakkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
-        throw false
-    }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
-    let caption = `
-${json.soal}
-
+  conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+  let id = m.chat
+  if (id in conn.tebakgambar) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakgambar.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `
+  ${json.deskripsi}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}teka untuk bantuan
+Ketik ${usedPrefix}hint untuk bantuan
 Bonus: ${poin} XP
-`.trim()
-    conn.tebakkata[id] = [
-        await conn.sendBL(m.chat, caption, wm, fla + 'tebak kata', [['Bantuan', '.teka']], m),
-        json, poin,
-        setTimeout(async () => {
-            if (conn.tebakkata[id]) await conn.sendB(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, null, [['Tebak Kata', '.tebakkata']], conn.tebakkata[id][0])
-            delete conn.tebakkata[id]
-        }, timeout)
-    ]
+    `.trim()
+  conn.tebakgambar[id] = [
+    await conn.sendBI(m.chat, caption, wm, json.img, [['Bantuan', '.hint']], m, {jpegThumbnail: await(await fetch(json.img)).buffer()}),
+    json, poin,
+    setTimeout(async () => {
+      if (conn.tebakgambar[id]) await conn.sendB(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, null, [['Tebak Gambar', '.tebakgambar']], conn.tebakgambar[id][0])
+      delete conn.tebakgambar[id]
+    }, timeout)
+  ]
 }
-handler.help = ['tebakkata']
+handler.help = ['tebakgambar']
 handler.tags = ['game']
-handler.command = /^tebakkata/i
+handler.command = /^tebakgambar/i
 
 module.exports = handler
