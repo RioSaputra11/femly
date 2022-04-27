beccf657882c42d4fe5f21759f0f4f7912c071ac
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-04-27 11:06:10 +0700

    Update siapakahaku_hint.js

--- plugins/siapakahaku_hint.js
+++ plugins/siapakahaku_hint.js
@@ -3,8 +3,9 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.siapakahaku)) throw false
     let json = conn.siapakahaku[id][1]
-    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.siapakahaku[id][0])
+    let ans = json.result.jawaban
+    let clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
+    m.reply('```' + clue + '```')
 }
 handler.command = /^who$/i
 handler.limit = true
