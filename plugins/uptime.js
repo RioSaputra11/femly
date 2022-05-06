815fa3e06217e5875f28fc0c15a3df79300db85b
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-07 00:10:36 +0700

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -1,7 +1,7 @@
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
     let _uptime = process.uptime() * 1000
     let uptimex = clockString(_uptime)
-    m.reply(uptimex)
+    conn.send3Button(m.chat, `Aktif selama:\n*${uptimex}*`.trim(), wm, 'bot stats', usedPrefix + 'stat', 'pinger', usedPrefix + 'ping', 'kembali ke menu', usedPrefix + 'menu', m)
 }
 handler.help = ['runtime']
 handler.tags = ['info']
@@ -10,8 +10,12 @@ handler.command = /^(uptime|runtime)$/i
 module.exports = handler
 
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return ['*' + h + ' Hour* ', '*' + m + ' Minute* ', '*' + s + ' Second*'].map(v => v.toString().padStart(2, 0)).join(' ')
+    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    let daysms = ms % (24 * 60 * 60 * 1000);
+    let hours = Math.floor((daysms) / (60 * 60 * 1000));
+    let hoursms = ms % (60 * 60 * 1000);
+    let minutes = Math.floor((hoursms) / (60 * 1000));
+    let minutesms = ms % (60 * 1000);
+    let sec = Math.floor((minutesms) / (1000));
+    return days + " Hari " + hours + " Jam " + minutes + " Menit " + sec + " Detik";
 }
